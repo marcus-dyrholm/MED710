71847ad356d519073be5fd26d09eec28dd4a151f
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2021-10-21 12:07:49 +0200

    push for backup purposes

--- Assets/Scripts/VelocityTesting.cs
+++ Assets/Scripts/VelocityTesting.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 using Valve.VR.InteractionSystem;
 
 public class VelocityTesting : MonoBehaviour
@@ -13,10 +14,17 @@ public class VelocityTesting : MonoBehaviour
     private GameObject hand;
     public Vector3 localVelocity;
     public float distance;
+    public float velocityRange;
+    public float velocityNewRange;
+
+    private GameObject _player;
+    
+    public SteamVR_Action_Boolean setdistance; //Grab Pinch is the trigger, select from inspecter
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;//which controller
 
     private void Start()
     {
-       
+        _player = GameObject.FindGameObjectWithTag("Player");
 
     }
 
@@ -26,29 +34,38 @@ public class VelocityTesting : MonoBehaviour
         //Debug.Log(distance);
 
         localVelocity = transform.InverseTransformDirection(this.GetComponent<HandPhysics>().handCollider.GetComponent<Rigidbody>().velocity);
-
+/*
         if (telekinesis.m_ActiveObject!= null && localVelocity.z >1)
         {
             //telekinesis.m_ActiveObject.GetComponent<Rigidbody>().AddForce(Vector3.forward);
             telekinesis.m_fDistance += 1;
             Debug.Log("throwing");
         }
-
+*/
         if (telekinesis.m_ActiveObject != null && telekinesis.m_fDistance >= 0)
         {
-            telekinesis.m_fDistance += map(localVelocity.z, -0.1f, 0.1f, -0.01f, 0.01f );
+            telekinesis.m_fDistance += map(localVelocity.z, -velocityRange, velocityRange, -velocityNewRange, velocityNewRange );
         }
 
         if (telekinesis.m_ActiveObject != null && telekinesis.m_fDistance < 0)
         {
-            telekinesis.m_fDistance -= map(localVelocity.z, -0.1f, 0.1f, -0.01f, 0.01f );
+            telekinesis.m_fDistance -= map(localVelocity.z, -velocityRange, velocityRange, -velocityNewRange, velocityNewRange );
         }
 
+        SteamVR_Input.actionsBoolean[1].GetStateDown(inputSource);
+
         //telekinesis.m_fDistance = map(localVelocity.z, -10, 10, -0.1f, 0.1f);
+    }
+
+    private void distanceSet()
+    {
+        distance = Vector3.Distance(this.transform.position, _player.transform.position);
     }
     
     float map(float s, float a1, float a2, float b1, float b2)
     {
         return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
     }
+    
+    
 }
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean SetDistance
+        {
+            get
+            {
+                return SteamVR_Actions.default_SetDistance;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
