cff1d765015f0655f25928f73832d83abc1a734a
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2021-11-28 16:52:21 +0100

    Tutorial done, still needs VR testing

---
+++ Assets/CollisionChecker.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChecker : MonoBehaviour
+{
+    public bool cubesIsColliding = false;
+    public int collisionID;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.GetComponent<TutorialCube>().ID == collisionID)
+            cubesIsColliding = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        cubesIsColliding = false;
+    }
+}
