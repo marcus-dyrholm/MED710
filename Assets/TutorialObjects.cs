bcd5b80617bb3b6d7bf81ec3169fa4598c4bcda7
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2021-11-28 15:26:55 +0100

    performance optimisation on environment, added tutorial scene

---
+++ Assets/TutorialObjects.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialObjects : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public Transform[] cubPos;
+    public List<GameObject> cubes;
+    private Rigidbody cubeRB;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            CreateCubes();
+        }
+    }
+
+    public void CreateCubes()
+    {
+        if (cubes.Count > 0)
+        {
+            for (int i = 0; i < cubes.Count; i++)
+            {
+                Destroy(cubes[i]);
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject cube = Instantiate(cubePrefab, cubPos[i]);
+            cubes.Add(cube);
+            cubeRB = cube.GetComponent<Rigidbody>();
+            cubeRB.mass = Random.Range((float)i + 1.0f, (float)i + 2.0f);
+        }
+        
+    }
+}
